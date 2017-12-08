e15401fdecf20eb8819208c48ab6d44922c5af0b
Rijn <bxbian951122@gmail.com>
2017-12-08 12:59:11 -0600

    push

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import App from './App';
 import jsforce from 'jsforce';
 
 if (process.env.NODE_ENV == 'development') {
-  var __sfdcSessionId = '00D2F0000008v9r!AQMAQGjTUcb6Rtf5AxPBdR2GSX7Yx.RsIC4i97LpHD3hBGqG4.DOgE.sJ5IcdnyNjviNayBovCAtJzKfCG1eHEngMoo2Sl6X';
+  window.__sfdcSessionId = '00D2F0000008v9r!AQMAQGjTUcb6Rtf5AxPBdR2GSX7Yx.RsIC4i97LpHD3hBGqG4.DOgE.sJ5IcdnyNjviNayBovCAtJzKfCG1eHEngMoo2Sl6X';
 }
 
-console.log(`session ${__sfdcSessionId}`);
+console.log(`session ${window.__sfdcSessionId}`);
 
 Vue.config.productionTip = false;
 
 var conn = new jsforce.Connection({
   // instanceUrl: 'https://c.cs91.visual.force.com',
-  // version: '41.0',
-  accessToken: __sfdcSessionId
+  version: '41.0',
+  accessToken: window.__sfdcSessionId
 });
 
-conn.metadata.describe('39.0', function (err, metadata) {
+conn.metadata.describe('41.0', function (err, metadata) {
   if (err) { return console.error('err', err); }
   console.log(metadata);
   for (var i = 0; i < metadata.length; i++) {
